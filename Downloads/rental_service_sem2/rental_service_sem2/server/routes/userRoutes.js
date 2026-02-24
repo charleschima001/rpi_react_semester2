f7e52dc40bf7c116c52175a5f9db6dc6c36c6a5a
finalproject <chukwuemekachima001@gmail.com>
2026-02-24 13:27:22 +0300

    Delete Downloads/rental_service_sem2/rental_service_sem2 directory

--- Downloads/rental_service_sem2/rental_service_sem2/server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express';
-import upload from '../middleware/upload.js';
-import { registration } from '../controllers/userController.js';
-
-const router = new Router();
-
-router.post('/registration', upload.single('avatar'), registration);
-
-export default router;
